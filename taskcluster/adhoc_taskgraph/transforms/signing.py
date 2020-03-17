2b00bf34942ddde18f3af06186254a661480dccd
Aki Sasaki <aki@escapewindow.com>
2020-03-16 18:42:31 -0700

    wip - add signing task

---
+++ taskcluster/adhoc_taskgraph/transforms/signing.py
@@ -0,0 +1,78 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
+Create signing tasks.
+"""
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from taskgraph.transforms.base import TransformSequence
+from taskgraph.util.schema import resolve_keyed_by
+from taskgraph.util.keyed_by import evaluate_keyed_by
+
+
+transforms = TransformSequence()
+
+
+@transforms.add
+def define_signing_flags(config, tasks):
+    for task in tasks:
+        dep = task["primary-dependency"]
+        # Current kind will be prepended later in the transform chain.
+        task["name"] = _get_dependent_job_name_without_its_kind(dep)
+        attributes = dep.attributes.copy()
+        if task.get("attributes"):
+            attributes.update(task["attributes"])
+        task["attributes"] = attributes
+        task["attributes"]["signed"] = True
+        if "run_on_tasks_for" in task["attributes"]:
+            task.setdefault("run-on-tasks-for", task["attributes"]["run_on_tasks_for"])
+
+        for key in ("worker-type", "worker.signing-type"):
+            resolve_keyed_by(
+                task,
+                key,
+                item_name=task["name"],
+                level=config.params["level"],
+            )
+        yield task
+
+
+@transforms.add
+def build_signing_task(config, tasks):
+    # TODO get manifest
+    for task in tasks:
+        dep = task["primary-dependency"]
+        task["dependencies"] = {"fetch": dep.label}
+        if not dep.task["payload"]["env"]["ARTIFACT_PREFIX"].startswith("public"):
+            scopes = task.setdefault('scopes', [])
+            scopes.append(
+                "queue:get-artifact:{}/*".format(dep.task["payload"]["env"]["ARTIFACT_PREFIX"].rstrip('/'))
+            )
+        # TODO match fetch task to manifest
+#        format = evaluate_keyed_by(
+#            config.graph_config["scriptworker"]["signing-format"],
+#            "signing-format",
+#            {
+#                "xpi-type": task["attributes"]["addon-type"],
+#                "kind": config.kind,
+#                "level": config.params["level"],
+#            }
+#        )
+#        # assert format in KNOWN_FORMATS
+#        task["worker"]["upstream-artifacts"] = [
+#            {
+#                "taskId": {"task-reference": "<fetch>"},
+#                "taskType": "build",
+#                "paths": paths,
+#                "formats": [format],
+#            }
+#        ]
+#        task.setdefault("extra", {})["xpi-name"] = dep.task["extra"]["xpi-name"]
+        del task["primary-dependency"]
+        yield task
+
+
+def _get_dependent_job_name_without_its_kind(dependent_job):
+    return dependent_job.label[len(dependent_job.kind) + 1:]
